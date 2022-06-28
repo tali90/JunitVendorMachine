d6297372b6371d8948f70229b227b4c2608c5b5a
tali90 <tayyub_ali1@outlook.com>
2022-06-29 00:37:20 +0100

    junit wallet test

--- src/test/java/vendormachine/users/util/WalletTEST.java
+++ src/test/java/vendormachine/users/util/WalletTEST.java
@@ -1,11 +1,35 @@
 package vendormachine.users.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class WalletTEST {
 
     @Test
-    public void test1(){
-
+    public void testAddCredit(){
+    	Wallet wallet1 = new Wallet();
+    	wallet1.addCredit(2f);
+    	assertEquals(2.5f, wallet1.getAllCredit());
+    }
+    
+    @Test
+    public void testGetCreditStored() {
+    	Wallet wallet1 = new Wallet();
+    	assertEquals(wallet1.getCredit(0.5f), 0.5f);
+    }
+    
+    @Test
+    public void testGetCreditExceed() {
+    	Wallet wallet1 = new Wallet();
+    	assertEquals(wallet1.getCredit(1.5f), 0.0f);
+    }
+    
+    @Test
+    public void testGetAllCredit() {
+    	Wallet wallet1 = new Wallet();
+    	wallet1.addCredit(2f);
+    	wallet1.addCredit(3f);
+    	assertEquals(5.5f,wallet1.getAllCredit());
     }
 }
